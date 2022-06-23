fdfa8187995e71dca99bd5b7d5f3666de93f996c
Thom Burt <thomburt@Thoms-MacBook-Pro.local>
2022-06-23 15:15:44 -0400

    m

--- client/src/components/hooks/config.js
+++ client/src/components/hooks/config.js
@@ -1,3 +1,4 @@
 export const API_BASE_URL = 'http://localhost:3001/api';
 
-export const HEROKU_LINK = 'https://flicks-and-food.herokuapp.com/api';
+export const HEROKU_LINK = 'https://flicksandfood.herokuapp.com/api';
+
